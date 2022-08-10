65d3ad89e2a0f3448574e8a72e26ab2fc16a4119
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-08-10 12:14:16 +0900

    added a simple index view page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,27 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show create]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurants).permit(:name, :address, :phone_number, :category)
+  end
 end
