79fa404bc65d196bf4b04185721a751b511f6131
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-08-10 11:45:43 +0900

    seeded restaurants to db

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "3.1.2"
 
 # Added by me
 gem "simple_form", github: "heartcombo/simple_form"
+gem "faker"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3", ">= 7.0.3.1"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Creating restaurants...'
+
+15.times do |restaurant|
+  # we need to use the Restaurant.create method to create a new restaurant
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+end
+
+puts '15 restaurants created!'
