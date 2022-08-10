8520fe612bca7b34a1bcc0c82d942a14104508e6
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-08-10 11:25:11 +0900

    created restaurant and review models, added validates to restaurant model

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+end
